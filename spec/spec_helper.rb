85e7b089efe68fb8b8d19a8134ef1908fde2585f
Marco Costa <mmarcottulio@gmail.com>
2016-12-20 01:06:08 -0500

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 require "bundler/setup"
 require "pry"
 require "pry-byebug"
